977566812b3a0affd98a6842ee462fa5b6a22257
danmeo <hutchinsond94@gmail.com>
2024-12-29 20:48:52 +0000

    \U00002728 New feature: Added timer functionality to report_results decorator, to see just how inefficient my code is

--- code/useful_functions.py
+++ code/useful_functions.py
@@ -1,4 +1,5 @@
 """Usefulish helper functions"""
+import time
 
 
 def input_data_read(filepath: str):
@@ -16,17 +17,24 @@ def input_data_read(filepath: str):
 
 def report_results(func):
     """Wrapper to return results in CLI.
+    Now with timer!
 
     Args:
         func (function): plop it in.
     """
 
     def wrapper(*args, **kwargs):
+        start_time = time.time()
         p1, p2 = func(*args, **kwargs)
+        end_time = time.time()
+        total_time = end_time - start_time
 
+        print()
         print("===== RESULTS =====")
         print(f"PART 1: {p1}")
         print(f"PART 2: {p2}")
+        print("-------------------")
+        print(f"Time Taken: {total_time:.2f} seconds")
         print("===================")
 
         return p1, p2
